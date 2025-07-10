5cb6512d31322ddcd0090c24c83e31de488713b5
bojesgovinda <bojespasaribu23@gmail.com>
2025-07-10 22:59:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My Portofolio | Bojes Govinda</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body id="menu">
+    <!-- Navbar -->
+     <nav class="navbar navbar-expand-lg bg-primary navbar-dark shadow-sm fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">Bojes Govinda Pasaribu</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#menu">Menu</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#tentang">tentang</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#proyek">Proyek</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#kontak">Kontak</a>
+        </li>
+      
+    </div>
+  </div>
+</nav>
+    <!-- Akhir Navbar -->
+
+    <!-- Jumbotron -->
+     <section class="jumbotron text-center">
+      <img src="img/iya.jpg" alt="BANGSAD KAU" width="200" class="rounded-circle img-thumbnail">
+      <h1 class="display-4">Bojes Govinda Pasaribu</h1>
+      <p class="lead">Mahasiswa | Teknologi Informasi</p>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f3f4f5" fill-opacity="1" d="M0,64L48,85.3C96,107,192,149,288,165.3C384,181,480,171,576,138.7C672,107,768,53,864,42.7C960,32,1056,64,1152,74.7C1248,85,1344,75,1392,69.3L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </section> 
+     <!-- Akhir Jumbotron -->
+
+      <!-- Tentang -->
+       <section id="tentang">
+        <div class="container">
+          <div class="row text-center mb-3">
+            <div class="col">
+              <h2>About Me</h2>
+            </div>
+          </div>
+          <div class="row justify-content-center fs-5 text-center">
+            <div class="col-md-4">
+              <p>Saya adalah seorang mahasiswa semester 2 di Universitas Pembangunan Panca Budi,Prodi saya adalah Teknologi Informasi yaitu prodi yang mempelajari tentang Rekayasa Perangkat Lunak</p>
+            </div>
+            <div class="col-md-4">
+              <p>Saat ini saya sedang mendalami PHP,HTML,CSS serta terus mengembangkan keterampilan saya melalui berbagai proyek pribadi dan latihan.Saya tertarik untuk berkarir di bidan teknologi dan siap berkontribusi dalam tim yang dinamis </p>
+            </div>
+          </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="rgb(161, 255, 255)" fill-opacity="1" d="M0,320L48,309.3C96,299,192,277,288,245.3C384,213,480,171,576,176C672,181,768,235,864,256C960,277,1056,267,1152,240C1248,213,1344,171,1392,149.3L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+       </section>
+       <!-- Akhir Tentang -->
+
+       <!-- Proyek -->
+        <section id="proyek">
+          <div class="container">
+            <div class="row text-center mb-3">
+              <div class="col">
+                <h2>Proyek Saya</h2>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-4 mb-3">
+                <div class="card">
+  <img src="project/11.jpg" class="card-img-top" alt="Project 1">
+  <div class="card-body text-center fw-bold">
+    <p class="card-text">To-Do List Web App</p>
+    <p class="card-text">Aplikasi berbasis web sederhana yang memungkinkan pengguna untuk mencatat daftar tugas, menandai tugas selesai, dan menghapus tugas. </p>
+    
+  </div>
+</div>
+              </div>
+              <div class="col-md-4 mb-3">
+                <div class="card">
+  <img src="project/22.jpg" class="card-img-top" alt="Project 2">
+  <div class="card-body text-center fw-bold">
+    <p class="card-text">Personal Portfolio Website</p>
+    <p class="card-text">Situs web statis yang menampilkan informasi pribadi, proyek, dan kontak. Dibuat menggunakan PHP,HTML, dan CSS.</p>
+  </div>
+</div>
+              </div>
+              <div class="col-md-4 mb-3">
+                <div class="card">
+  <img src="project/44.jpg" class="card-img-top" alt="Project 4">
+  <div class="card-body text-center fw-bold">
+    <p class="card-text">Aplikasi Manajemen Keuangan Pribadi</p>
+    <p class="card-text">Proyek ini membantu saya memahami struktur data, pengolahan file, dan logika akuntansi dasar.</p>
+  </div>
+</div>
+              </div>
+              </div>
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f3f4f5" fill-opacity="1" d="M0,96L48,117.3C96,139,192,181,288,192C384,203,480,181,576,176C672,171,768,181,864,197.3C960,213,1056,235,1152,240C1248,245,1344,235,1392,229.3L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+        </section>
+        <!-- Akhir Proyek -->
+
+        <!-- Kontak -->
+         <section id="kontak">
+          <div class="container">
+            <div class="row text-center">
+              <div class="col">
+                <h2>Kontak Saya</h2>
+              </div>
+            </div>
+            <div class="row justify-content-center">
+              <div class="col-md-6">
+                <form>
+  <div class="mb-3">
+    <label for="name" class="form-label">Nama Lengkap</label>
+    <input type="text" class="form-control" id="name" aria-describedby="name">
+  </div>
+  <div class="mb-3">
+    <label for="email" class="form-label">Email</label>
+    <input type="email" class="form-control" id="email" aria-describedby="email">
+  </div>
+  <div class="mb-3">
+  <label for="pesan" class="form-label">Pesan</label>
+  <textarea class="form-control" id="Pesan" rows="3"></textarea>
+</div>
+
+  <button type="submit" class="btn btn-primary">Kirim </button>
+</form>
+              </div>
+            </div>
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d6efd" fill-opacity="1" d="M0,256L48,240C96,224,192,192,288,197.3C384,203,480,245,576,272C672,299,768,309,864,282.7C960,256,1056,192,1152,176C1248,160,1344,192,1392,208L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+         </section>
+         <!-- Akhir Kontak -->
+          
+         <!-- Footer -->
+          <footer class="bg-primary text-white text-center ">
+            <p>Created with <i class="bi bi-calendar-heart text-danger"></i>  by <a href="https://www.instagram.com/govinda_ytta/" class="text-white fw-bold">Bojes Govinda</a></p>
+          </footer>
+          <!-- Akhir Footer -->
+    </body>
+</html>
